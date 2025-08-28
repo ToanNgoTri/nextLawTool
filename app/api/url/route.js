b055eb9b7d6879a3b4b2cdd4f101774e710567e6
ToanNgoTri <161665438+ToanNgoTri@users.noreply.github.com>
2025-08-28 15:07:13 +0700

    a

--- app/api/url/route.js
+++ app/api/url/route.js
@@ -1,9 +1,17 @@
+export const runtime = "nodejs";
 const puppeteer = require("puppeteer");
+var cors = require("cors");
+const bodyParer = require("body-parser");
 
-import { NextResponse } from "next/server";
-export async function GET() {
+import { NextResponse,Request } from "next/server";
 
-    
+export async function GET(request) {
+
+      const { searchParams } = new URL(request.url);
+      // const url = searchParams.get("url");
+      const url = "https://luatvietnam.vn/tin-van-ban-moi/da-co-nghi-dinh-232-2025-nd-cp-sua-doi-nghi-dinh-ve-quan-ly-hoat-dong-kinh-doanh-vang-186-103775-article.html#google_vignette"
+
+
 async function eachRun(url) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
@@ -14,153 +22,19 @@ async function eachRun(url) {
     let source = await page.content({ waitUntil: "domcontentloaded" });
   
     const r = await page.evaluate(async () => {
-      let bg_phantich = document.querySelectorAll(".bg_phantich"); // loại bỏ phần tử khong cần thiết
-      for (let f = 0; f < bg_phantich.length; f++) {
-        bg_phantich[f].remove();
-      }
   
-      let elementContent = document.querySelectorAll(
-        ".noidungtracuu >.docitem-1:not(.docitem-9 ~ div), .docitem-2:not(.docitem-9 ~ div), .docitem-5:not(.docitem-9 ~ div), .docitem-11:not(.docitem-9 ~ div), .docitem-12:not(.docitem-9 ~ div)"
-        // ".noidungtracuu .docitem-5"
-      );
-      console.log("elementContent", elementContent);
-  
-      let lawRelated = "";
-      let roleSign = "";
-  
-      if (Object.keys(elementContent).length == 0) {
-        elementContent = document.querySelectorAll(".noidungtracuu");
-        lawRelated = "";
-        roleSign = "";
-      } else {
-        lawRelated = document.querySelector("#chidanthaydoind >.docitem-14")
-          ? document.querySelector("#chidanthaydoind >.docitem-14").innerText
-          : "";
-        lawRelated =
-          lawRelated +
-          "\n" +
-          (document.querySelector("#chidanthaydoind >.docitem-15")
-            ? document.querySelector("#chidanthaydoind >.docitem-15").innerText
-            : "");
-        lawRelated = lawRelated.replace(/\_*/g, "");
-        lawRelated = lawRelated.replace(/\n+/g, "\n");
-  
-        roleSign = document.querySelector("#chidanthaydoind >.docitem-9")
-          ? document.querySelector("#chidanthaydoind >.docitem-9").innerText
-          : "";
-        roleSign = roleSign.replace(/\u00A0/gim, " ");
-        roleSign = roleSign.replace(/\n +/g, "\n");
-        roleSign = roleSign.replace(/\n+/g, "\n");
-      }
-  
-      console.log(elementContent[1]);
   
       var content = "";
-      for (let a = 0; a < elementContent.length; a++) {
-        // content = content + "\n" + elementContent[a] ?elementContent[a].innerText:"";
-        content = content + "\n" + elementContent[a].innerText;
-      }
-      content = content.replace(/\n+/g, "\n");
-      content = content.replace(/  /gm, " ");
+      content = document.querySelector("#chidanthaydoind >.docitem-14").innerText
+      // for (let a = 0; a < elementContent.length; a++) {
+      //   // content = content + "\n" + elementContent[a] ?elementContent[a].innerText:"";
+      //   content = content + "\n" + elementContent[a].innerText;
+      // }
   
-      let tableInfomation = document.querySelector(".div-table")
-        ? document.querySelector(".div-table").innerText
-        : "";
-  
-      let lawNumber;
-      let unitPublish;
-      let lawKind;
-      let nameSign;
-      let lawDaySign;
-      let lawDescription;
-      lawDescription = document.querySelector(".the-document-summary")
-        ? document.querySelector(".the-document-summary").innerText
-        : "";
-  
-      lawDescription = lawDescription.replace(/^ */, "");
-      if (tableInfomation.match(/VBHN/)) {
-        lawNumber = document.querySelector(
-          ".div-table tr:nth-child(1) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(1) td:nth-child(2)")
-              .innerText
-          : "";
-        lawNumber = lawNumber.replace(/(^ | $)/gim, "");
-        lawNumber = lawNumber.match(/^\d\//gim) ? `0${lawNumber}` : lawNumber;
-  
-        unitPublish = document.querySelector(
-          ".div-table tr:nth-child(2) td:nth-child(4)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(2) td:nth-child(4)")
-              .innerText
-          : "";
-  
-        lawKind = document.querySelector(
-          ".div-table tr:nth-child(2) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(2) td:nth-child(2)")
-              .innerText
-          : "";
-  
-        nameSign = document.querySelector(
-          ".div-table tr:nth-child(3) td:nth-child(4)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(3) td:nth-child(4)")
-              .innerText
-          : "";
-  
-        lawDaySign = document.querySelector(
-          ".div-table tr:nth-child(1) td:nth-child(4)"
-        ).innerText;
-      } else {
-        lawNumber = document.querySelector(
-          ".div-table tr:nth-child(2) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(2) td:nth-child(2)")
-              .innerText
-          : "";
-        lawNumber = lawNumber.replace(/(^ | $)/gim, "");
-        lawNumber = lawNumber.match(/^\d\//gim) ? `0${lawNumber}` : lawNumber;
-  
-        unitPublish = document.querySelector(
-          ".div-table tr:nth-child(1) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(1) td:nth-child(2)")
-              .innerText
-          : "";
-  
-        lawKind = document.querySelector(
-          ".div-table tr:nth-child(3) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(3) td:nth-child(2)")
-              .innerText
-          : "";
-  
-        nameSign = document.querySelector(
-          ".div-table tr:nth-child(3) td:nth-child(4)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(3) td:nth-child(4)")
-              .innerText
-          : "";
-  
-        lawDaySign = document.querySelector(
-          ".div-table tr:nth-child(4) td:nth-child(2)"
-        )
-          ? document.querySelector(".div-table tr:nth-child(4) td:nth-child(2)")
-              .innerText
-          : "";
-      }
   
       return {
         content,
-        lawNumber,
-        unitPublish,
-        lawKind,
-        nameSign,
-        lawDaySign,
-        lawDescription,
-        lawRelated,
-        roleSign,
+
       };
   
     });
@@ -169,7 +43,7 @@ async function eachRun(url) {
     return r;
   }
   
-  let data = await eachRun("https://luatvietnam.vn/dat-dai/nghi-dinh-230-2025-nd-cp-quy-dinh-cac-truong-hop-khac-duoc-mien-giam-tien-su-dung-dat-tien-thue-dat-408916-d1.html")
+  let data = await eachRun(url)
     
     return NextResponse.json({ data });        
   
--- app/once/page.js
+++ app/once/page.js
@@ -1,74 +1,154 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
 
-import styles from '../page.module.css'
+import styles from "../page.module.css";
 
 export default function Page() {
+  const [URL, setURL] = useState("");
 
-
+  async function receive() {
+    console.log(URL);
     
-
-
-
-
-
-
+    fetch(`/api/url?url=${URL}`).then((res) =>
+      res.json().then((res) => console.log(res.data))
+    );
+  }
 
   return (
     <div id={styles.container}>
+      <textarea
+        className={styles.input_area}
+        id={styles.url}
+        value={URL}
+        onChange={(e) => setURL(e.target.value)}
+      ></textarea>
+
       <div id={styles.inner_container}>
         <div id={styles.input_container}>
           <p>lawNumber</p>
-          <textarea className={styles.input_area}  id={styles.lawNumber}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.lawNumber}
+          ></textarea>
           <p>unitPublish</p>
-          <textarea className={styles.input_area} id={styles.unitPublish}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.unitPublish}
+          ></textarea>
           <p>lawKind</p>
-          <textarea className={styles.input_area} id={styles.lawKind}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.lawKind}
+          ></textarea>
           <p>nameSign</p>
-          <textarea className={styles.input_area} id={styles.nameSign}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.nameSign}
+          ></textarea>
           <p>lawDaySign</p>
-          <textarea className={styles.input_area} id={styles.lawDaySign}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.lawDaySign}
+          ></textarea>
           <p>lawDescription</p>
-          <textarea className={styles.input_area} id={styles.lawDescription}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.lawDescription}
+          ></textarea>
           <p>lawRelated</p>
-          <textarea className={styles.input_area} id={styles.lawRelated}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.lawRelated}
+          ></textarea>
           <p>roleSign</p>
-          <textarea className={styles.input_area} id={styles.roleSign}></textarea>
-          <textarea className={styles.input_area} id={styles.content_input}></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.roleSign}
+          ></textarea>
+          <textarea
+            className={styles.input_area}
+            id={styles.content_input}
+          ></textarea>
         </div>
-        <div className={{...styles.navi_container,...styles.navi_input}}>
-          <button type="button" className={styles.navi_btb} onclick="goToStartInput()">
+        <div className={{ ...styles.navi_container, ...styles.navi_input }}>
+          <button
+            type="button"
+            className={styles.navi_btb}
+            onClick={() => goToStartInput()}
+          >
             Go to Start
           </button>
 
-          <button type="button" className={styles.navi_btb} onclick="goToEndInput()">
+          <button
+            type="button"
+            className={styles.navi_btb}
+            onClick={() => goToEndInput()}
+          >
             Go to End
           </button>
         </div>
 
-        <div className={{...styles.navi_container,...styles.navi_output}}>
-          <button type="button" className={styles.navi_btb} onclick="goToStartOutput()">
+        <div className={{ ...styles.navi_container, ...styles.navi_output }}>
+          <button
+            type="button"
+            className={styles.navi_btb}
+            onClick={() => goToStartOutput()}
+          >
             Go to Start
           </button>
 
-          <button type="button" className={styles.navi_btb} onclick="goToEndOutput()">
+          <button
+            type="button"
+            className={styles.navi_btb}
+            onClick={() => goToEndOutput()}
+          >
             Go to End
           </button>
         </div>
 
         <div className={styles.btb_container}>
-          <button type="button" className={styles.btb} onclick="getInfo()">
+          <button
+            type="button"
+            className={styles.btb}
+            onClick={() => receive()}
+          >
+            Receive
+          </button>
+
+          <button
+            type="button"
+            className={styles.btb}
+            onClick={() => getInfo()}
+          >
             Get Infomation
           </button>
-          <button className={{...styles.btb,...styles.copy_btb}} onclick="convertContent()">
+          <button
+            className={{ ...styles.btb, ...styles.copy_btb }}
+            onClick={() => convertContent()}
+          >
             Get Content
           </button>
-          <button className={{...styles.btb,...styles.delete_btb}} onclick="Push()">Push</button>
-          <button className={{...styles.btb,...styles.naviNext_btb}} onclick="NaviNext()">Next</button>
-          <button className={{...styles.btb,...styles.back_btb}} onclick="NaviHome()">Back</button>
+          <button
+            className={{ ...styles.btb, ...styles.delete_btb }}
+            onClick={() => Push()}
+          >
+            Push
+          </button>
+          <button
+            className={{ ...styles.btb, ...styles.naviNext_btb }}
+            onClick={() => NaviNext()}
+          >
+            Next
+          </button>
+          <button
+            className={{ ...styles.btb, ...styles.back_btb }}
+            onClick={() => NaviHome()}
+          >
+            Back
+          </button>
         </div>
         <textarea className={styles.output} cols="90"></textarea>
       </div>
     </div>
-  )
+  );
 }
-
