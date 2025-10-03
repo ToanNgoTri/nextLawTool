2f8c5010e5ff55728cce1441c210bb5ffefa5190
ToanNgoTri <161665438+ToanNgoTri@users.noreply.github.com>
2025-10-03 21:12:18 +0700

    a

--- app/check/page.js
+++ app/check/page.js
@@ -19,6 +19,38 @@ function Page() {
       });
   }
 
+
+ async function checkNghiDinh() {
+  setURL("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=ngh%E1%BB%8B&search=&search=&DocTypeIds=11&OrganIds=0&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1")
+        let a = await fetch(`/api/check?url=` + encodeURIComponent("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=ngh%E1%BB%8B&search=&search=&DocTypeIds=11&OrganIds=0&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1"))
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.content);
+        // console.log("res.data", res.content);
+      });
+  }
+
+   async function checkThongTu() {
+    setURL("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=&search=&search=&DocTypeIds=21&DocTypeIds=22&OrganIds=0&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1")
+        let a = await fetch(`/api/check?url=` + encodeURIComponent("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=&search=&search=&DocTypeIds=21&DocTypeIds=22&OrganIds=0&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1"))
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.content);
+        // console.log("res.data", res.content);
+      });
+  }
+
+   async function checkVanBanHopNhat() {
+    setURL("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=v%C4%83&search=v%C4%83n%20ph%C3%B2ng%20q&search=&DocTypeIds=59&OrganIds=325&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1")
+        let a = await fetch(`/api/check?url=` + encodeURIComponent("https://luatvietnam.vn/van-ban/tim-van-ban.html?keywords=&SearchOptions=1&SearchByDate=issueDate&DateFromString=01/01/2025&DateToString=&search=v%C4%83&search=v%C4%83n%20ph%C3%B2ng%20q&search=&DocTypeIds=59&OrganIds=325&FieldIds=0&LanguageId=0&SignerIds=0&SignerIds=0&RowAmount=20&PageIndex=1"))
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.content);
+        // console.log("res.data", res.content);
+      });
+  }
+
+
   return (
     <div id={styles.container}>
       <div id={styles.inner_container}>
@@ -33,17 +65,45 @@ function Page() {
             style={{ width: 500 }}
             cols={100}
           ></textarea>
-
+<div>
           <button
-            style={{ width: "20%", marginTop: 10 }}
+            style={{ width: "20%", marginTop: 10,marginRight:20, height:30 }}
             onClick={() => check()}
           >
             Check
           </button>
-
+          <button
+            style={{ width: "20%", marginTop: 10,marginRight:20, height:30 }}
+            onClick={() => checkNghiDinh()}
+          >
+            Check Nghị Định
+          </button>
+          <button
+            style={{ width: "20%", marginTop: 10,marginRight:20, height:30 }}
+            onClick={() => checkThongTu()}
+          >
+            Check Thông Tư
+          </button>
+          <button
+            style={{ width: "20%", marginTop: 10,marginRight:20, height:30 }}
+            onClick={() => checkVanBanHopNhat()}
+          >
+            Check Văn bản hợp nhất
+          </button>
+</div>
           <table style={{ paddingTop: 10 }}>
             <tbody>
               <tr>
+                <td
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "white",
+                    borderStyle: "solid",
+                    padding: 5,
+                  }}
+                >
+                  STT
+                </td>
                 <td
                   style={{
                     borderWidth: 1,
@@ -82,6 +142,16 @@ function Page() {
                           borderWidth: 1,
                           borderColor: "white",
                           borderStyle: "solid",
+                          textAlign: "center",}}
+                      >
+                        {i+1}
+                      </td>
+                      <td
+                        style={{
+                          borderWidth: 1,
+                          borderColor: "white",
+                          borderStyle: "solid",
+                          padding: 10,
                         }}
                       >
                         {key}
@@ -99,8 +169,10 @@ function Page() {
                           borderWidth: 1,
                           borderColor: "white",
                           borderStyle: "solid",
+                          backgroundColor: "#4CAF50",
                         }}>
-                      <a href={`/once?URL=${data[key]}`} target="_blank">
+                      <a href={`/once?URL=${data[key]}`} target="_blank"
+                      style={{justifyContent: 'center',display: 'flex'}}>
                         Redirect
                       </a>
 
